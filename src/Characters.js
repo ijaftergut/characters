371a3d432768f55807a2fad4dd74f1d47545db81
Isaac Aftergut <ijagut@vt.edu>
2023-11-06 17:57:49 -0500

    characters display by user

--- src/Characters.js
+++ src/Characters.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Characters = ({ characters, auth})=> {
+  console.log(auth.id)
   return (
     <div>
       <h2>Characters</h2>
       <ul>
-        {
-          characters.map( character => {
+        {characters.filter(character=>character.user_id===auth.id).map( character => {
+          console.log(character.user_id)
            
             return (
               <li key={ character.id }>
